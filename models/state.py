a7a5655e8d3027285de10c3d6b95a4f740701775
keza-laura <k.laura@alustudent.com>
2026-06-21 19:15:00 +0200

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State class"""
+
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class that represents a state"""
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    # DBStorage relationship
+    if models.storage_t == 'db':
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete, delete-orphan"
+        )
+
+    # FileStorage fallback
+    else:
+        @property
+        def cities(self):
+            """Returns list of City objects linked to this State"""
+            city_list = []
+
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
